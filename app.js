4a58bba41b9df5166250a6cf1fe9d8deb0a3fcbb
Ugur Armagan <ugurarmagan93@gmail.com>
2017-10-06 16:58:44 +0000

    add mongolab

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var commentRoutes       = require("./routes/comments"),
     campgroundRoutes    = require("./routes/campgrounds"),
     indexRoutes         = require("./routes/index");
     
-mongoose.connect("mongodb://localhost/yelp_camp_v11");
+//mongoose.connect("mongodb://localhost/yelp_camp_v11");
+mongoose.connect("mongodb://ugur:kaya159@ds111535.mlab.com:11535/ua_yelpcamp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
