6da9e2a068d25771b389f8ed740d3ea95fc4bc40
Ugur Armagan <ugurarmagan93@gmail.com>
2017-10-09 12:40:05 -0500

    fix error in heroku server

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
-var express     = require("express"),
-    app         = express(),
-    bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose"),
-    flash       = require("connect-flash"),
-    passport    = require("passport"),
-    LocalStrategy=require("passport-local"),
-    methodOverride = require("method-override"),
-    Campground  = require("./models/campground"),
-    Comment     = require("./models/comment"),
-    User        = require("./models/user"),
-    seedDB      = require("./seeds");
+var express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose"),
+  flash = require("connect-flash"),
+  passport = require("passport"),
+  LocalStrategy = require("passport-local"),
+  methodOverride = require("method-override"),
+  Campground = require("./models/campground"),
+  Comment = require("./models/comment"),
+  User = require("./models/user"),
+  seedDB = require("./seeds");
 
-    //requiring routes
-var commentRoutes       = require("./routes/comments"),
-    campgroundRoutes    = require("./routes/campgrounds"),
-    indexRoutes         = require("./routes/index");
+//requiring routes
+var commentRoutes = require("./routes/comments"),
+  campgroundRoutes = require("./routes/campgrounds"),
+  indexRoutes = require("./routes/index");
 
 var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp"
 mongoose.connect(url);
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
@@ -28,9 +30,9 @@ app.use(flash());
 
 //PASSPORT CONFIGURATION
 app.use(require("express-session")({
-    secret:"Sabah is the smartest girl in the world",
-    resave: false,
-    saveUninitialize:false
+  secret: "Sabah is the smartest girl in the world",
+  resave: false,
+  saveUninitialize: false
 }));
 
 app.use(passport.initialize());
@@ -39,18 +41,19 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req,res,next){
-    res.locals.currentUser = req.user;
-    res.locals.error = req.flash("error");
-    res.locals.success = req.flash("success");
-    next();
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
 });
 
-app.use("/",indexRoutes);
+app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes)
-app.use("/campgrounds/:id/comments",commentRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
 
 
-app.listen(8080, function(){
-   console.log("The YelpCamp Server Has Started");
+app.listen(process.env.PORT || 8080, function() {
+  var port = server.address().port;
+  console.log("The YelpCamp Server Has Started on port " + port);
 });
