a836b7ee3817a1e0e3b1100e45d9e9dda0fba238
Ugur Armagan <ugurarmagan93@gmail.com>
2017-10-09 21:08:57 -0500

    delete all files in campgrounds db

--- seeds.js
+++ seeds.js
@@ -4,28 +4,28 @@ var Comment   = require("./models/comment");
 
 var data = [
     {
-        name: "Cloud's Rest", 
+        name: "Cloud's Rest",
         image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
         description: "blah blah blah"
     },
     {
-        name: "Desert Mesa", 
+        name: "Desert Mesa",
         image: "https://farm4.staticflickr.com/3859/15123592300_6eecab209b.jpg",
         description: "blah blah blah"
     },
     {
-        name: "Canyon Floor", 
+        name: "Canyon Floor",
         image: "https://farm1.staticflickr.com/189/493046463_841a18169e.jpg",
         description: "blah blah blah"
     }
 ]
 
-function seedDB(){
-   //Remove all campgrounds
-   Campground.remove({}, function(err){
-        if(err){
-            console.log(err);
-        }
+// function seedDB(){
+//    //Remove all campgrounds
+//    Campground.remove({}, function(err){
+//         if(err){
+//             console.log(err);
+//         }
         // console.log("removed campgrounds!");
         //  //add a few campgrounds
         // data.forEach(function(seed){
@@ -51,7 +51,7 @@ function seedDB(){
         //         }
         //     });
         // });
-    }); 
+    });
     //add a few comments
 }
 
